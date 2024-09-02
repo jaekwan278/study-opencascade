9c9ef4e3583f29d5061bac33f93e302d8e62a1c9
Jaekwan <wprhks536@gmail.com>
2024-09-02 20:37:03 +0900

    ...

---
+++ readStep/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <STEPControl_Reader>
+
+/*
+1. Step 파일 읽기 (AP203, AP214 우선 구현.)
+*/
+
+int main(int, char**){
+    std::cout << "Hello, from readStep!\n";
+    STEPControl_Reader
+}
