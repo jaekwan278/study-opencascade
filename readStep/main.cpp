026f05ba9ee53dd3b90769fc8ad15ab799e7b2a7
Jaekwan <wprhks536@gmail.com>
2024-09-04 01:00:59 +0900

    feat : read Step

--- readStep/main.cpp
+++ readStep/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <STEPControl_Reader.hxx>
+#include <Interface_Static.hxx>
 
-// 1. Step 파일 읽기
 
 using namespace std;
 int main(){
-    // 파일 경로
+
     Standard_CString filename = "";
+
     // 파일읽기 관련 에러 메시지 mode : STEP 엔티티당 모든 메시지의 순차적 목록 제공.
     IFSelect_PrintCount mode = IFSelect_ItemsByEntity;
 
-    // 읽기
     STEPControl_Reader reader;
+    // 파일 읽기와 데이터 전송 작업의 상태를 나타내는 열거형.
     IFSelect_ReturnStatus status = reader.ReadFile(filename);
 
-    // 파일 읽기 관련 에러 메시지, ()
-    reader.PrintCheckLoad(true, mode);
+    if ( status == IFSelect_RetDone){
+        cout << "@ read success" << endl;
+    }else if(status == IFSelect_RetError){
+        cout << "@ read fail" << endl;
+    }else if(status == IFSelect_RetVoid){
+        cout << "empty file" << endl;
+    }
+
+    // 파일 읽기 관련 에러 메시지, (true = 실패 메시지만, false = 모든 메시지)
+    reader.PrintCheckLoad(false, mode);
+
+    // OCCT 매핑에 사용할 데이터 추출
+    // 1. 정밀도 값. ( Interfacd_Static )
+    Standard_Integer ic = Interface_Static::IVal("read.precision.mode");
 
     return 0;
 }
